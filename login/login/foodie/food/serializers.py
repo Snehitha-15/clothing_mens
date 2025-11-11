f3e67154195669f4386d325eb815356c90660084
Snehitha-15 <snehitha@dideccanindia.com>
2025-11-11 12:19:29 +0530

    Removed Backend

--- login/login/foodie/food/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['fullname', 'email']
--- login/login/foodie/food/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('register/', views.register),
-    path('send-otp/', views.send_otp),
-    path('login/', views.verify_otp),
-    path('logout/', views.logout),
-]
