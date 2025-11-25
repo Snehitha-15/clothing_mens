71c2b268f5ce4e5715b88f564e15ad70b7c3b387
poojitha <poojithapothap@gmail.com>
2025-11-25 16:06:52 +0530

    commitchanges

--- backend/dist/mensclothingbackend/settings.py
+++ backend/dist/mensclothingbackend/settings.py
@@ -97,12 +97,18 @@ TEMPLATES = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = [
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
-#CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 
 WSGI_APPLICATION = 'mensclothingbackend.wsgi.application'
 
