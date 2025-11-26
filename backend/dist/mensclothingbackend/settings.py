36ae9aec0c45e3702fda085f5980411c649f3260
poojitha <poojithapothap@gmail.com>
2025-11-26 10:36:20 +0530

    didchanges

--- backend/dist/mensclothingbackend/settings.py
+++ backend/dist/mensclothingbackend/settings.py
@@ -204,10 +204,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
 }
+    
+    '''DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}'''
 
 AUTHENTICATION_BACKENDS = [
     'mensclothingfrontend.backends.EmailOrPhoneBackend',
